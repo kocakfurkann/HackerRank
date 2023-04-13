eff0b7def7ef278d76ef284613c33074782c3fab
kocakfurkann <130656136+kocakfurkann@users.noreply.github.com>
2023-04-13 14:22:37 +0300

    Create 02-java-stdin-and-stdout-i.java

---
+++ 02-java-stdin-and-stdout-i.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner tarayici = new Scanner(System.in);
+        int a = tarayici.nextInt();
+        int b = tarayici.nextInt();
+        int c = tarayici.nextInt();
+        
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+    }
+}
